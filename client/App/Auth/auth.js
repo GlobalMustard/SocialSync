eb5d2ecbb7fca675f399457b049796ce57832ac1
Joe Lowinske <lowi0008@gmail.com>
2014-08-29 16:36:15 -0700

    (doc) Added pseudo-pseudo-code to JS files

--- client/App/Auth/auth.js
+++ client/App/Auth/auth.js
@@ -1,1 +1,10 @@
-auth.js
+/*
+DESCRIPTION: module for authorization interface
+
+CONTROLLER:
+-on click, initiate authorization for given SM (this may include login on SocialSync OR SM website...unclear right now)
+-after login, render notifications view
+
+FACTORY:
+-??
+*/
---
+++ client/App/Notifications/notifications.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION: module for all social media notifications
+
+CONTROLLER:
+-create $scope variable for social media data, which is fed by api-services
+-should update and clear on button click (functions)
+
+FACTORY:
+-??
+*/
