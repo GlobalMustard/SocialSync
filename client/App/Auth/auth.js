563c3e1308b26bba01dffbbc764b21366af63063
Joe Lowinske <lowi0008@gmail.com>
2014-09-02 13:26:38 -0500

    (feat) basic html setup, angular controllers set up

--- client/App/Auth/auth.js
+++ client/App/Auth/auth.js
@@ -1,9 +1,78 @@
+angular.module('socialsync.auth', [])
+
+.factory('Auth', function($http, $state) {
+  var login = function(user) {
+    return $http({
+      method: 'POST', 
+      url: '', //SERVER URLS??
+      data: user,
+    })
+    .then(function(resp) {
+      return resp.data.token;
+    })
+    .error(function(err) {
+      //indicate invalid login in auth view
+    });
+  };
+
+  var signup = function(user) {
+    return $http({
+      method: 'POST',
+      url: '',  //GET URL INFO
+      data: user
+    })
+    .then(function(resp) {
+      return resp.data.token;
+    })
+    .error(function(err) {
+      //indicate invald signup in auth view
+    });
+  }
+
+  var isAuth = function() {
+    return $http({
+      method: 'GET', 
+      url: '', //GET URL INFO
+    })
+    .then()
+    .error(function(err) {
+      //redirect to login
+      //indicate 'you need to login' in auth view state
+    });
+  }
+
+  return {
+    login: login,
+    signup: signup,
+    isAuth: isAuth
+  };
+})
+
+.controller('AuthController', function($scope, $location, Auth) { //DO WE NEED LOCATION WITH UI ROUTER??
+  $scope.user = {};
+  
+  $scope.login = function(user) {
+    Auth.login($scope.user).then(function() {
+      //navigate to notifications view state
+      $location.path('/notifications');
+    });
+  };
+
+  $scope.signup = function(user) {
+    Auth.signup($scope.user).then(function() {
+      //navigate to notifications view state
+      $location.path('/notifications');
+    })
+  };
+});
+
+
 /*
 DESCRIPTION: module for authorization interface
 
 CONTROLLER:
 -on click, initiate authorization for given SM (this may include login on SocialSync OR SM website...unclear right now)
--after login, render notifications view
+-after login, render notifications view (factory more appropriate??)
 
 FACTORY:
 -??
